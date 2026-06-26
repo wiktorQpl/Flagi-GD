880e06544759ae72e98881716ef2d973936d9b9c
wiktorQpl <wlazlowiktor14@gmail.com>
2026-06-26 18:55:56 +0200

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,27 +3,27 @@
 
 using namespace geode::prelude;
 
-struct FlagColor {
-    cocos2d::ccColor3B color1;
-    cocos2d::ccColor3B color2;
-};
-
-FlagColor getFlagColors(std::string const& flagName) {
-    if (flagName == "Polska") return { {255, 255, 255}, {214, 40, 40} };
-    if (flagName == "Ukraina") return { {0, 91, 187}, {255, 213, 0} };
-    if (flagName == "Wlochy") return { {0, 146, 70}, {206, 43, 55} };
-    if (flagName == "LGBT") return { {255, 0, 0}, {0, 0, 255} };
-    return { {255, 255, 255}, {255, 255, 255} };
-}
-
 class $modify(MyPlayer, SimplePlayer) {
     void updatePlayerFrame() {
         SimplePlayer::updatePlayerFrame();
+
         auto selectedFlag = Mod::get()->getSettingValue<std::string>("selected-flag");
-        if (selectedFlag != "Wylaczone") {
-            FlagColor colors = getFlagColors(selectedFlag);
-            this->setColor(colors.color1);
-            this->setSecondColor(colors.color2);
+
+        if (selectedFlag == "Polska") {
+            this->setColor({255, 255, 255});
+            this->setSecondColor({214, 40, 40});
+        } 
+        else if (selectedFlag == "Ukraina") {
+            this->setColor({0, 91, 187});
+            this->setSecondColor({255, 213, 0});
+        } 
+        else if (selectedFlag == "Wlochy") {
+            this->setColor({0, 146, 70});
+            this->setSecondColor({206, 43, 55});
+        } 
+        else if (selectedFlag == "LGBT") {
+            this->setColor({255, 0, 0});
+            this->setSecondColor({0, 0, 255});
         }
     }
 };
